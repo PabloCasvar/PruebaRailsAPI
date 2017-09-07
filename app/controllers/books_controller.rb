c093606f7f9600def501084a70c05866e4eba0ad
PabloCasvar <casvar.pablo@gmail.com>
2017-09-07 10:35:26 -0500

    conexión a la base de datos MySQL

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,28 @@
 class BooksController < ApplicationController
+    
+    def index
+        @books = Book.all
+    end
+    
+    def show
+        @book = Book.find(params[:id])
+    end   
+    
     def new
     end
 
     def create
-        render plain: params[:book].inspect
+        @book = Book.new(book_params)
+
+        @book.save
+        #redirect_to @book
+        redirect_to books_path, id: @book.id
+    end
+
+
+
+    private 
+    def book_params
+        params.require(:book).permit(:title, :author)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170906080030) do
-
-  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "title"
-    t.string "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.string "author"
